443337b073bad38bb84b1b15f3951f3c05acb7ee
zerma <zermaslan@gmail.com>
2013-09-02 14:16:15 -0500

    Added column values for droping eggs

--- chans.py
+++ chans.py
@@ -1,6 +1,8 @@
 import pyglet
 from juego import recursos
 
+cols = [270,390,525]
+
 ventana =  pyglet.window.Window(800, 600)
 
 puntos_label = pyglet.text.Label(text="Puntos: 0", x=10, y=575)
@@ -15,7 +17,7 @@ mini_fail_1 = pyglet.sprite.Sprite(img=recursos.mini_egg_fail, x=730, y=580)
 mini_fail_2 = pyglet.sprite.Sprite(img=recursos.mini_egg_fail, x=755, y=580)
 mini_fail_3 = pyglet.sprite.Sprite(img=recursos.mini_egg_fail, x=780, y=580)
 
-huevo = pyglet.sprite.Sprite(img=recursos.egg, x=400, y=300)
+huevo = pyglet.sprite.Sprite(img=recursos.egg, x=525, y=300)
 ch_izq = pyglet.sprite.Sprite(img=recursos.chansey_left, x=300, y=184)
 ch_der = pyglet.sprite.Sprite(img=recursos.chansey_right, x=500, y=184)
 ch_cen = pyglet.sprite.Sprite(img=recursos.chansey_center, x=400, y=177)
@@ -31,7 +33,7 @@ def on_draw():
 	nivel_label.draw()
 	fails_label.draw()
 
-	mini_fail_1.draw()
+	mini_1.draw()
 	mini_2.draw()
 	mini_3.draw()
 
